bcf29d35305188959a80b3a5213cc854a29cccef
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-10-06 06:07:53 -0400

    Add prettier formatting on save

--- Frontend/src/Login.tsx
+++ Frontend/src/Login.tsx
@@ -1,21 +1,31 @@
-import React, {useState} from 'react';
-import {Button} from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
 
 const Login: React.FC = (): JSX.Element => {
-	const [username, setUsername] = useState<string>('');
-	const [password, setPassword] = useState<string>('');
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
 
-	return (
-		<div>
-			<input type='text' placeholder='Enter Username' value={username} onChange={e => {
-				setUsername(e.target.value);
-			}} />
-			<input type='password' placeholder='Enter Password' value={password} onChange={e => {
-				setPassword(e.target.value);
-			}} />
-			<Button variant='primary'>Login</Button>
-		</div>
-	);
-};
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Enter Username"
+                value={username}
+                onChange={(e) => {
+                    setUsername(e.target.value)
+                }}
+            />
+            <input
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                }}
+            />
+            <Button variant="primary">Login</Button>
+        </div>
+    )
+}
 
-export default Login;
+export default Login
