661bac161fe991e08c69d69f186dfd990bf60318
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-10-06 18:54:20 -0400

    Update folder structure and resolve conflict between eslint and prettier

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
+import Login from './components/login/Login';
 
 const App = () => (
 	<div className='App'>
