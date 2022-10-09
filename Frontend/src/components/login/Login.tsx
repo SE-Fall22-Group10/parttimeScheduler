e0772205fde8333862ad386aee861bab4db4fe15
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-10-09 13:08:36 -0400

    Update background for login component

--- Frontend/src/components/login/Login.tsx
+++ Frontend/src/components/login/Login.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Container} from 'react-bootstrap';
+import {Button, Container, Row} from 'react-bootstrap';
 import type {LoginProps, UserDetailsObject} from '../../interface';
 import {verifyUserLogin} from '../../utils';
 // Import loginCss from './Login.module.css';
@@ -31,30 +31,39 @@ const Login: React.FC<LoginProps> = (props: LoginProps): JSX.Element => {
 	};
 
 	return (
-		<Container>
-			<input
-				type='text'
-				placeholder='Enter Email Id'
-				value={userEmail}
-				onChange={e => {
-					setUserEmail(e.target.value);
-				}}
-			/>
-			<input
-				type='password'
-				placeholder='Enter Password'
-				value={password}
-				onChange={e => {
-					setPassword(e.target.value);
-				}}
-			/>
-			<Button
-				variant='primary'
-				onClick={handleLogin}
-			>
+		<Container style={{display: 'block', height: '50vh', width: '50%', backgroundColor: '#1DBFB8', alignItems: 'center', textAlign: 'center', marginTop: '5%'}}>
+			<div>
+				<input
+					type='text'
+					placeholder='Enter Email Id'
+					value={userEmail}
+					onChange={e => {
+						setUserEmail(e.target.value);
+					}}
+					style={{margin: '13% 5% 0 5%'}}
+				/>
+			</div>
+			<div>
+				<input
+					type='password'
+					placeholder='Enter Password'
+					value={password}
+					onChange={e => {
+						setPassword(e.target.value);
+					}}
+					style={{margin: '2%'}}
+				/>
+			</div>
+			<div>
+				<Button
+					variant='primary'
+					onClick={handleLogin}
+					style={{margin: '2%'}}
+				>
         Login
-			</Button>
-			{loginMessage}
+				</Button>
+				{loginMessage}
+			</div>
 		</Container>
 	);
 };
