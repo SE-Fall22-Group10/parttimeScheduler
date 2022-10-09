0e3df9725ccdd2e67758b5d404cd6626c4a00faf
shlokio <112107528+shlokio@users.noreply.github.com>
2022-10-09 19:18:16 -0400

    Add notif and request functionality

--- app/routes/userShifts.js
+++ app/routes/userShifts.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const Store = require("../models/store")
-const Req = require("../models/requests")
+const Store = require("../models/store");
+const Req = require("../models/requests");
 var authenticateToken = require("../middleware/authenticateToken");
 var adminAuth = require("../middleware/adminAuth");
 const auth = require("../middleware/authentication");
@@ -18,36 +18,43 @@ router.post("/addShift", async (req, res) => {
     const shiftFrom = new Date(req.body.shiftFrom);
     const shiftTill = new Date(req.body.shiftTill);
     const storeName = req.body.storeName;
-    var weekNumber = 4*shiftFrom.getMonth()+~~(shiftFrom.getDate()/7)+1;
+    var weekNumber = 4 * shiftFrom.getMonth() + ~~(shiftFrom.getDate() / 7) + 1;
     const sch = {
       weekNumber: weekNumber,
-      shiftArray:[{
-      "shiftFrom": shiftFrom,
-      "shiftTill": shiftTill,
-      "shiftHours": shiftTill.getHours()-shiftFrom.getHours(),
-      "storeName": storeName}]
+      shiftArray: [
+        {
+          shiftFrom: shiftFrom,
+          shiftTill: shiftTill,
+          shiftHours: shiftTill.getHours() - shiftFrom.getHours(),
+          storeName: storeName,
+        },
+      ],
     };
     const sch1 = {
-      "shiftFrom": shiftFrom,
-      "shiftTill": shiftTill,
-      "shiftHours": shiftTill.getHours()-shiftFrom.getHours(),
-      "storeName": storeName
-    }
-    
+      shiftFrom: shiftFrom,
+      shiftTill: shiftTill,
+      shiftHours: shiftTill.getHours() - shiftFrom.getHours(),
+      storeName: storeName,
+    };
+
     // console.log(sch);
 
     if (!shiftFrom || !shiftTill) {
-      return res.status(400).send({ Message: "Time not in required format!"});
+      return res.status(400).send({ Message: "Time not in required format!" });
     }
     // Send response in here
     var f = await User.findOne({ email: req.body.email });
     var flag = 0;
-    for(let shift of f["shifts"])
-    { if(shift['weekNumber']==weekNumber)
-    {shift['shiftArray'].push(sch1); flag=1;}}
-    if(flag==0)
-    {f.shifts.push(sch);}
-    f.totalShiftHours += shiftTill.getHours()-shiftFrom.getHours();
+    for (let shift of f["shifts"]) {
+      if (shift["weekNumber"] == weekNumber) {
+        shift["shiftArray"].push(sch1);
+        flag = 1;
+      }
+    }
+    if (flag == 0) {
+      f.shifts.push(sch);
+    }
+    f.totalShiftHours += shiftTill.getHours() - shiftFrom.getHours();
     await f.save();
     res.status(200).send(f);
   } catch (e) {
@@ -63,46 +70,48 @@ router.post("/addShift", async (req, res) => {
 // POST-method
 // update shift details for a user
 router.post("/updateShift", async (req, res) => {
-    try{
-        const shiftFrom = new Date(req.body.shiftFrom);
-        const shiftTill = new Date(req.body.shiftTill);
-        const storeName = req.body.storeName;
-        const shiftId = req.body.shiftId;
-        const sch = { 
-            shiftFrom: shiftFrom, 
-            shiftTill: shiftTill,
-            storeName: storeName
-        };
-        
-        if (!shiftFrom || !shiftTill) {
-            return res.status(400).send({Message: "Time not in required format!"});
-        }
-        var f = await User.findOne({email: req.body.email});
-        for(let shift of f["shifts"]){
-            // console.log(shift["_id"] == shiftId);
-            if(shift["_id"] == shiftId){
-                shift["shiftFrom"] = shiftFrom;
-                shift["shiftTill"] = shiftTill;
-                shift["storeName"] = storeName;
-                f.shiftHours = shiftTill.getHours()-shiftFrom.getHours();
-                if (f.shiftHours>20) {
-                  return res.status(400).send({ Message: "Shift Hours exceeded! Cannot Add Shift"});
-                }
-                var week = shiftFrom.getDate()
-                f.weekNumber = ~~(week/7)+1;
-                break;               
-            }
-        }
-        f.save()
-        res.status(200).send(f);
-    }catch (e) {
-        if (e.code === 11000) {
-            res.status(409).send({ Message: "Shift not found" });
-        } else {
-        console.log(e);
-        res.status(400).send(e);
-        }
+  try {
+    const shiftFrom = new Date(req.body.shiftFrom);
+    const shiftTill = new Date(req.body.shiftTill);
+    const storeName = req.body.storeName;
+    const shiftId = req.body.shiftId;
+    const sch = {
+      shiftFrom: shiftFrom,
+      shiftTill: shiftTill,
+      storeName: storeName,
+    };
+
+    if (!shiftFrom || !shiftTill) {
+      return res.status(400).send({ Message: "Time not in required format!" });
     }
+    var f = await User.findOne({ email: req.body.email });
+    for (let shift of f["shifts"]) {
+      // console.log(shift["_id"] == shiftId);
+      if (shift["_id"] == shiftId) {
+        shift["shiftFrom"] = shiftFrom;
+        shift["shiftTill"] = shiftTill;
+        shift["storeName"] = storeName;
+        f.shiftHours = shiftTill.getHours() - shiftFrom.getHours();
+        if (f.shiftHours > 20) {
+          return res
+            .status(400)
+            .send({ Message: "Shift Hours exceeded! Cannot Add Shift" });
+        }
+        var week = shiftFrom.getDate();
+        f.weekNumber = ~~(week / 7) + 1;
+        break;
+      }
+    }
+    f.save();
+    res.status(200).send(f);
+  } catch (e) {
+    if (e.code === 11000) {
+      res.status(409).send({ Message: "Shift not found" });
+    } else {
+      console.log(e);
+      res.status(400).send(e);
+    }
+  }
 });
 
 //POST-method
@@ -112,18 +121,21 @@ router.post("/offerShift", async (req, res) => {
     console.log(req.body);
     const shiftId = req.body.shiftId;
     var f = await User.findOne({ email: req.body.email });
-    for(let shift of f["shifts"]){
+    for (let shift of f["shifts"]) {
       // console.log(shift["_id"] == shiftId);
-      if(shift["_id"] == shiftId){
-          shift["shiftToggle"] = 1;
-          var reqs = new Req({
-            offerer: req.body.email,
-            grabbed: 0          });
-          await reqs.save();
-          console.log(reqs)
-          break;               
+      if (shift["_id"] == shiftId) {
+        shift["shiftToggle"] = "Up for grabs";
+        var reqs = new Req({
+          offerer: req.body.email,
+          grabbed: 0,
+          shiftId: shiftId,
+          storeName: storeName
+        });
+        await reqs.save();
+        console.log(reqs);
+        break;
       }
-  }
+    }
     //Saving Req object to the db.
     //send index instead of obj id
     await f.save();
@@ -147,22 +159,22 @@ router.post("/applyBid", async (req, res) => {
     const takeremail = req.body.takeremail;
     var f = await User.findOne({ email: req.body.email });
     var sch = 0;
-    for(let shift of f["shifts"]){
+    for (let shift of f["shifts"]) {
       // console.log(shift["_id"] == shiftId);
-      if(shift["_id"] == shiftId){
-          shift["shiftForGrabsStatus"] = "Shift taken";
-          break;               
+      if (shift["_id"] == shiftId) {
+        shift["shiftForGrabsStatus"] = "Shift taken";
+        break;
       }
-  }
-  var g = await User.findOne({ email: takeremail});
-  console.log(f);
-  console.log(g);
-  g.shifts.push(sch);
-  var reqs = await Req.findOne({shiftid: shiftId});
-  reqs['grabbed'] = 1;
-  reqs['taker'] = takeremail;
-  //Saving Req object to the db.
-  await reqs.save();
+    }
+    var g = await User.findOne({ email: takeremail });
+    console.log(f);
+    console.log(g);
+    g.shifts.push(sch);
+    var reqs = await Req.findOne({ shiftid: shiftId });
+    reqs["grabbed"] = 1;
+    reqs["taker"] = takeremail;
+    //Saving Req object to the db.
+    await reqs.save();
     //send index instead of obj id
     await f.save();
     await g.save();
@@ -187,19 +199,36 @@ router.post("/tradeShift", async (req, res) => {
     var g = await User.findOne({ email: req.body.email2 });
     var ff = f.shifts;
     var gg = g.shifts;
-    if (ff[req.body.indexOfShift1].storeName != gg[req.body.indexOfShift2].storeName) {
-      return res.status(400).send({Message: "Shift exchange not authorized!"});
+    if (
+      ff[req.body.indexOfShift1].storeName !=
+      gg[req.body.indexOfShift2].storeName
+    ) {
+      return res
+        .status(400)
+        .send({ Message: "Shift exchange not authorized!" });
     }
 
-    [ff[req.body.indexOfShift1].shiftFrom, gg[req.body.indexOfShift2].shiftFrom] = [gg[req.body.indexOfShift2].shiftFrom,ff[req.body.indexOfShift1].shiftFrom];
-    
-    [ff[req.body.indexOfShift1].shiftTill, gg[req.body.indexOfShift2].shiftTill] = [gg[req.body.indexOfShift2].shiftTill,ff[req.body.indexOfShift1].shiftTill];
+    [
+      ff[req.body.indexOfShift1].shiftFrom,
+      gg[req.body.indexOfShift2].shiftFrom,
+    ] = [
+      gg[req.body.indexOfShift2].shiftFrom,
+      ff[req.body.indexOfShift1].shiftFrom,
+    ];
+
+    [
+      ff[req.body.indexOfShift1].shiftTill,
+      gg[req.body.indexOfShift2].shiftTill,
+    ] = [
+      gg[req.body.indexOfShift2].shiftTill,
+      ff[req.body.indexOfShift1].shiftTill,
+    ];
     ff[req.body.indexOfShift1].shiftForGrabsStatus = "Not for grabs";
     gg[req.body.indexOfShift2].shiftForGrabsStatus = "Not for grabs";
     //send index instead of obj id
     await f.save();
     await g.save();
-    res.status(200).send({Message: "Done"});
+    res.status(200).send({ Message: "Done" });
   } catch (e) {
     if (e.code === 11000) {
       res.status(409).send({ Message: "Shift not found" });
@@ -217,13 +246,13 @@ router.delete("/removeShift", async (req, res) => {
     console.log(req.body);
     const shiftId = req.body.shiftId;
     var f = await User.findOne({ email: req.body.email });
-    for(let shift of f["shifts"]){
-      if(shift["_id"] == shiftId){
-          f["shifts"].pop(shift);
-          console.log(shift);
-          break;               
+    for (let shift of f["shifts"]) {
+      if (shift["_id"] == shiftId) {
+        f["shifts"].pop(shift);
+        console.log(shift);
+        break;
       }
-  }
+    }
     //send index instead of obj id
     await f.save();
     res.status(200).send(f);
@@ -239,43 +268,40 @@ router.delete("/removeShift", async (req, res) => {
 
 // POST-method
 // Remove an employee from the store
-router.post('/removeEmployeeFromStore', adminAuth, async(req, res) =>{
-  try{
-      const {
-          employeeEmail,
-          storeName
-      } = req.body
-      var user = await User.findOne({ email: employeeEmail });
-      if(user != null){
-        for(let shift of user["shifts"]){
-            if(shift["storeName"] == storeName){                
-                shift["shiftToggle"] = 1;
-            }
-        };
-        await user.save();
-        var store = await Store.findOne({ storeName: storeName});
-        if(store != null){
-          for(let employee of store["employees"]){
-            console.log(employee.employeeEmail, employeeEmail);
-            if(employee["employeeEmail"]===employeeEmail)
-            employee.stillWorksForStore = false;
-          }
-          await store.save();
-          res.status(200).send({ Message: "User removed" });
-        }else{
-          res.status(409).send({ Message: "Store not found" });
+router.post("/removeEmployeeFromStore", adminAuth, async (req, res) => {
+  try {
+    const { employeeEmail, storeName } = req.body;
+    var user = await User.findOne({ email: employeeEmail });
+    if (user != null) {
+      for (let shift of user["shifts"]) {
+        if (shift["storeName"] == storeName) {
+          shift["shiftToggle"] = 1;
         }
-      }else{        
-        res.status(409).send({ Message: "User does not work for the store" });
       }
-  } catch(e) {
-      if(e.code === 11000){
-          res.status(409).send({ Message: "Shift not found" })
-      }else{
-          console.log(e)
-          res.status(400).send(e)
+      await user.save();
+      var store = await Store.findOne({ storeName: storeName });
+      if (store != null) {
+        for (let employee of store["employees"]) {
+          console.log(employee.employeeEmail, employeeEmail);
+          if (employee["employeeEmail"] === employeeEmail)
+            employee.stillWorksForStore = false;
+        }
+        await store.save();
+        res.status(200).send({ Message: "User removed" });
+      } else {
+        res.status(409).send({ Message: "Store not found" });
       }
+    } else {
+      res.status(409).send({ Message: "User does not work for the store" });
+    }
+  } catch (e) {
+    if (e.code === 11000) {
+      res.status(409).send({ Message: "Shift not found" });
+    } else {
+      console.log(e);
+      res.status(400).send(e);
+    }
   }
-})
+});
 
 module.exports = router;
