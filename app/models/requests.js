fb3b1fedf9c958c58379e100487ddf3ef57f52c8
shlokio <112107528+shlokio@users.noreply.github.com>
2022-10-09 19:49:18 -0400

    Updated requests

--- app/models/requests.js
+++ app/models/requests.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const RequestsSchema = new Schema({
     offerer: { type: String, required: true, text: true },
     grabbed: { type: Number, required: false, enum: [0,1],default:0 },
-    storename: { type: String, required: false, text: true,default:"" },
+    storeName: { type: String, required: false, text: true,default:"" },
     taker: { type: String, required: true, text: true },
     shiftId: { type: String, required: true, text: true }
 });
